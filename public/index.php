436265d96370fb4d641875c5278ff36358d077e7
Юрий Грушевский <jurapro@yandex.ru>
2021-07-15 21:57:19 +0700

    Fix bug in php 7.4

--- public/index.php
+++ public/index.php
@@ -1,6 +1,12 @@
 <?php
 //Включаем запрет на неявное преобразование типов
 declare(strict_types=1);
+
+//Включаем отображение всех ошибок и предупреждений
+ini_set('error_reporting', (string)E_ALL);
+ini_set('display_errors', (string)1);
+ini_set('display_startup_errors', (string)1);
+
 //Включаем сессии на все страницы
 session_start();
 
